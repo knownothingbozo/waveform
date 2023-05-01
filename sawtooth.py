760bc3806a7dc49a119a077ada2f53bd40de44db
knownothingbozo <calebkin@gmail.com>
2023-05-01 09:51:36 -0600

    Update sawtooth.py

--- sawtooth.py
+++ sawtooth.py
@@ -9,30 +9,32 @@ fig, axs = plt.subplots()
 axs.clear()
 
 ## Variables that define the wave
-start = 0
+start = -10
 end = 10
-period = 2 
-steps_per_period = 4   ##This can't go below 4
+period = 5
+steps_per_period = 57   ##This can't go below 4
                         ## It is possible to program functionality for 3 steps per period
-                        ## Uncomment below to implement
+                        ## But it isn't prctical to impliment here
 stepnumber = (steps_per_period * period) + 1
-amplitude = 2
-direction = 1
+amplitude = 4
+direction = 0
 ##
 
 def sawchomper(start, end, stepnumber, period, amplitude, direction):
     x = np.linspace(start, end, stepnumber)
     y = []
     Nst = (stepnumber - 1)
-    step = (end - start)/ Nst
+#     step = (end - start)/ Nst
+    
     v = (Nst / period)
     if v % 2 != 1:
         v+=1
+    inc = amplitude / ((v-1)/2)
         
-    print(v)    ## Uncomment for debug
-    print(Nst)
+#     print(v)    ## Uncomment for debug
+#     print(Nst)
     
-    if direction == 1:
+    if direction == 0:
         j = 0
         l = 0
         m = 0
@@ -40,44 +42,58 @@ def sawchomper(start, end, stepnumber, period, amplitude, direction):
 #             print(l)    ## Uncomment for debug
             if l == 0:
                 y.append(j)
-                j += step
+                j += inc
                 l += 1
                 m = 0
             elif l <= (v-1)/2:
                 y.append(j)
-                j += step
+                j += inc
+                l += 1
+            elif l > (v-1)/2 and l < (v-1):
+            ##elif l > (v-1)/2 and l <= (v-1):  ##first step to 3 step functionality
+                if m == 0:
+                    j = -amplitude
+                    m += 1
+                y.append(j)
+                j += inc
+                l += 1
+            else:     
+                y.append(j)
+                j += inc
+                l = 0
+            
+            
+    elif direction == 1:
+        j = 0
+        l = 0
+        m = 0
+        for i in x:
+            if l == 0:
+                y.append(j)
+                j -= inc
+                l += 1
+                m = 0
+            elif l <= (v-1)/2:
+                y.append(j)
+                j -= inc
                 l += 1
             elif l > (v-1)/2 and l < (v-1):
             ##elif l > (v-1)/2 and l <= (v-1):  ##uncomment to add 3 step functionality
                 if m == 0:
-                    j = -step*(v-1)/2
+                    j = amplitude
                     m += 1
                 y.append(j)
-                j += step
+                j -= inc
                 l += 1
             else:     
                 y.append(j)
-                j += step
+                j -= inc
                 l = 0
-            
-            
-    elif direction == 0:
-        j = period
-        for i in x:
-            if i < k:
-                y.append((j/period) * amplitude)
-                j -= step
-            else:
-                
-                j = period
-                y.append((j/period) * amplitude)
-                j -= step
-                k += period
                 
     return x, y
         
 x, y = sawchomper(start, end, stepnumber, period, amplitude, direction)
 axs.plot(x, y)
 
-print(x)    ## Uncomment for debug
-print(y)
+# print(x)    ## Uncomment for debug
+# print(y)
